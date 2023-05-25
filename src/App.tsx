f4ef112f6cef8cf4d4f5c873cae8ae4df927e086
Mubaraq Mustapha <ayomidemusty@gmail.com>
2023-05-25 14:27:19 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { Box, Grid, Stack } from "@mui/material";
+import "./App.scss";
+import { CreditCard } from "@mui/icons-material";
+import CardForm from "./components/CardForm";
+
+function App() {
+  return (
+    <Grid
+      container
+      width="95%"
+      mx="auto"
+      my="50px"
+      height="auto"
+      minHeight="500px"
+      bgcolor="#ffffff"
+      p={{ xs: "2em 1em", sm: "4em 3em" }}
+    >
+      <Grid item className="item-one" xs={12} sm={7}>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb="2em"
+        >
+          <Stack direction="row" alignItems="center" gap="15px">
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              width="40px"
+              height="40px"
+              bgcolor="#025efe"
+              borderRadius="50%"
+            >
+              <CreditCard sx={{ color: "#ffffff", m: "auto" }} />
+            </Stack>
+            <h1>
+              <span>AceCoin</span>Pay
+            </h1>
+          </Stack>
+
+          <Stack direction="row" gap="5px" alignItems="center">
+            <span className="clock">0</span>
+            <span className="clock">1</span>
+            <span>:</span>
+            <span className="clock">1</span>
+            <span className="clock">9</span>
+          </Stack>
+        </Stack>
+
+        <CardForm />
+      </Grid>
+      <Grid item className="item-one" xs={12} sm={5}></Grid>
+    </Grid>
+  );
+}
+
+export default App;
---
+++ src/components/CardForm.tsx
@@ -0,0 +1,96 @@
+import { CheckCircle, Edit, ViewCompact } from "@mui/icons-material";
+import { Box, Stack } from "@mui/material";
+import masterCard from "../assets/icons8-mastercard.svg";
+
+const CardForm = () => {
+  return (
+    <Stack direction="column" className="card-form-container" spacing="30px">
+      <Box>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Box>
+            <h3>Card Number</h3>
+            <p>Enter the 16-digit card number on the card</p>
+          </Box>
+          <Stack direction="row" alignItems="center" gap="10px">
+            <Edit sx={{ color: "#025efe" }} />
+            <p>Edit</p>
+          </Stack>
+        </Stack>
+        <div className="input-container">
+          <img src={masterCard} alt="mastercard" />
+          <input
+            type="text"
+            placeholder="2412   -   7512    -   3412   -   3456"
+          />
+          <CheckCircle sx={{ color: "#025efe" }} />
+        </div>
+      </Box>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent={{ xs: "flex-start", sm: "space-between" }}
+        gap={{ xs: 0, sm: "30px" }}
+        alignItems={{ xs: "flex-start", sm: "center" }}
+      >
+        <Box>
+          <h3>CVV Number</h3>
+          <p>Enter the 3 or 4-digit number on the card</p>
+        </Box>
+        <Box width={{ xs: "100%", sm: "50%" }}>
+          <div className="input-container input-centre">
+            <input type="text" defaultValue="327" />
+            <ViewCompact />
+          </div>
+        </Box>
+      </Stack>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent={{ xs: "flex-start", sm: "space-between" }}
+        gap={{ xs: 0, sm: "30px" }}
+        alignItems={{ xs: "flex-start", sm: "center" }}
+      >
+        <Box>
+          <h3>Expiry date</h3>
+          <p>Enter the expiration date of the card</p>
+        </Box>
+        <Stack
+          direction="row"
+          gap="10px"
+          alignItems="center"
+          width={{ xs: "100%", sm: "50%" }}
+        >
+          <div className="input-container">
+            <input type="text" defaultValue="09" />
+          </div>
+          /
+          <div className="input-container">
+            <input type="text" defaultValue="22" />
+          </div>
+        </Stack>
+      </Stack>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent={{ xs: "flex-start", sm: "space-between" }}
+        gap={{ xs: 0, sm: "30px" }}
+        alignItems={{ xs: "flex-start", sm: "center" }}
+      >
+        <Box>
+          <h3>Password</h3>
+          <p>Enter your Dynamic Password</p>
+        </Box>
+        <Box width={{ xs: "100%", sm: "50%" }}>
+          <div className="input-container">
+            <input type="password" defaultValue="1245365474" />
+            <ViewCompact />
+          </div>
+        </Box>
+      </Stack>
+      <button>Pay Now</button>
+    </Stack>
+  );
+};
+
+export default CardForm;
